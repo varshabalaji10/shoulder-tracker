69df23cfcd1d74c12819729a04e8f5dff92a17bb
Varsha Balaji <47684000+varshabalaji10@users.noreply.github.com>
2025-07-15 14:24:51 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
